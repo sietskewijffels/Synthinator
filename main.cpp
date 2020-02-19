d2ee06b391cccf2489c776a6fc8620aae56b1d91
CptFrikadel <adkupper@gmail.com>
2020-02-19 23:14:47 +0100

    Removed obsolete includes

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
-#include <alsa/asoundlib.h>
-#include <stdio.h>
-#include <thread>
-#include <stdlib.h>
-#include <string>
-#include <array>
 
-#include "oscillator.hpp"
 #include "InputThread.hpp"
-#include "keyboard.hpp"
 #include "EventQueue.hpp"
 #include "AudioThread.hpp"
 
 
 int main (int argc, char *argv[]){
-    
+
     // TODO: make log file instead of stderr
     std::cerr << "Yo Whaddup!" << std::endl;
 
