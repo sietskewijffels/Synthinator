d7e6394fe70e3f09e1e17b056e53f6894d9d8bb2
CptFrikadel <adkupper@gmail.com>
2020-05-18 00:31:51 +0200

    Implemented FrameBuffer in Oscillator

--- src/oscillator.cpp
+++ src/oscillator.cpp
@@ -4,9 +4,6 @@
 Oscillator::Oscillator(const float _norm_freq):
     norm_freq(_norm_freq)
 {
-
-
-  buffer = (float*) malloc(sizeof(float) * 512);
 }
 
 
@@ -19,54 +16,27 @@ Oscillator::Oscillator(const float _norm_freq, WaveType _type):
 
   switch (_type){
     case WaveType::WAVE_SINE:
-      waveform = new SineWave(norm_freq, 512); //TODO: fix magic number
+      waveform = new SineWave(norm_freq);
       break;
     case WaveType::WAVE_SQUARE:
-      waveform = new SquareWave(norm_freq, sizeof(buffer));
+      waveform = new SquareWave(norm_freq);
       break;
   default:
-      waveform = new SineWave(norm_freq, sizeof(buffer));
+      waveform = new SineWave(norm_freq);
       break;
 
   }
 
-  buffer = (float*) malloc(sizeof(float) * 512);
 
 }
 
-Oscillator::Oscillator(const float _norm_freq, float * _buffer, WaveType _type):
-    norm_freq(_norm_freq),
-    buffer(_buffer),
-    type(_type)
+Oscillator::~Oscillator(){
 
-
-{
-
-
-    switch (_type){
-    case WaveType::WAVE_SINE:
-        waveform = new SineWave(norm_freq, sizeof(buffer));
-        break;
-    case WaveType::WAVE_SQUARE:
-        waveform = new SquareWave(norm_freq, sizeof(buffer));
-        break;
-    default:
-        waveform = new SineWave(norm_freq, sizeof(buffer));
-        break;
-
-    }
+    delete waveform;
 }
 
-Oscillator::Oscillator(const float _norm_freq, float * _buffer):
-    norm_freq(_norm_freq),
-    buffer(_buffer)
+FrameBuffer& Oscillator::oscillate(){
 
-
-{
-}
-
-void Oscillator::oscillate(){
-
-  waveform->generate(buffer);
+  return waveform->generate();
 
 }
--- src/oscillator.hpp
+++ src/oscillator.hpp
@@ -1,9 +1,8 @@
 /*
-  Holds components for a VCO.
+  Holds components for an arbitrary waveform oscillator.
 
-  oscillate() fills the buffer with samples
+  oscillate() fills the buffer with samples and returns a reference
 
-  Buffer can be pre-allocated, or if NULL is passed it will allocate one.
 */
 
 #ifndef _OSCILLATOR_HPP
@@ -11,6 +10,7 @@
 
 #include <cstdlib>
 #include "waveform.hpp"
+#include "FrameBuffer.hpp"
 
 
 
@@ -19,16 +19,15 @@ class Oscillator {
 public:
 
   Oscillator(const float norm_freq);
-  Oscillator(const float norm_freq, float * _buffer);
   Oscillator(const float norm_freq, WaveType _type);
-  Oscillator(const float norm_freq, float * buffer, WaveType _type);
-public:
+  ~Oscillator();
+
+  FrameBuffer buffer;
+
   Waveform * waveform;    // Waveform gen
   float norm_freq;        // Normalized frequency (cycles / sample)
-  float * buffer;         // Sample Buffer
 
-public:
-  void oscillate();       // Do oscillate callback thingy
+  FrameBuffer& oscillate();       // Do oscillate callback thingy
 
 public:
   void setType(WaveType _type){ type = _type;}
